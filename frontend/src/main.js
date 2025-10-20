c05996454b211e65d74ee1d8e42e53daeaa141c6
demostanis <demostanis@protonmail.com>
2025-10-20 02:31:12 +0200

    selfhost sour-gummy font

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import { createWebHistory, createRouter } from "vue-router";
+import "@fontsource/sour-gummy/400.css";
+import "@fontsource/sour-gummy/500.css";
+import "@fontsource/sour-gummy/800.css";
 import "./style.css";
 import App from "./App.vue";
 import routes from "./routes";
